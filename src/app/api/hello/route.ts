0c054795f7e358a8547d4be7d3c7f01670aceb98
Ben Ho <tounsoo@gmail.com>
2023-04-08 13:32:25 -0700

    api test with notion

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,15 @@
-export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+import { Client } from "@notionhq/client";
+import { NextResponse } from "next/server";
+
+const secret = process.env.NOTION_KEY;
+const databaseId = process.env.NOTION_DATABASE_ID;
+
+export async function GET(res: Response) {
+  const notion = new Client({ auth: secret });
+  if (!secret || !databaseId)
+    throw new Error("Missing notion secret or DB-ID.");
+
+  const query = await notion.databases.query({ database_id: databaseId });
+
+  return NextResponse.json(query.results);
 }
