3eab2382a0c927be40916da91de192efbc821702
Ben Ho <tounsoo@gmail.com>
2023-04-09 16:31:27 -0700

    change how data is fetched

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,29 @@
+import { Client } from "@notionhq/client";
+
 async function getData() {
-  const res = await fetch("http://localhost:3000/api/hello");
+  const secret = process.env.NOTION_KEY;
+  const databaseId = process.env.NOTION_DATABASE_ID;
+  const notion = new Client({ auth: secret });
+  if (!secret || !databaseId)
+    throw new Error("Missing notion secret or DB-ID.");
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
+  const query = await notion.databases.query({ database_id: databaseId });
 
-  return res.json();
+  return query;
 }
 export default async function Home() {
   const data = await getData();
   return (
     <div>
       <ul>
-        {data.map((project: typeof data, i: number) => {
-          const title = project.properties["Name"].title[0].text.content;
-          return <li key={title}>{title}</li>;
+        {data.results.map((project) => {
+          if ("properties" in project) {
+            if ("title" in project.properties.Name) {
+              const title = project.properties.Name.title[0].plain_text;
+              const id = project.id;
+              return <li key={id}>{title}</li>;
+            }
+          }
         })}
       </ul>
     </div>
