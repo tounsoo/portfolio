4ca6a17d8c40399ec255ea83fc0f4f45585b6184
Ben Ho <tounsoo@gmail.com>
2023-04-11 10:10:08 -0700

    update: add under construction message

--- app/page.tsx
+++ app/page.tsx
@@ -2,32 +2,47 @@ import { Client } from "@notionhq/client";
 
 export const revalidate = 600;
 
-async function getData() {
-  const secret = process.env.NOTION_KEY;
-  const databaseId = process.env.NOTION_DATABASE_ID;
-  const notion = new Client({ auth: secret });
-  if (!secret || !databaseId)
-    throw new Error("Missing notion secret or DB-ID.");
+// async function getData() {
+//   const secret = process.env.NOTION_KEY;
+//   const databaseId = process.env.NOTION_DATABASE_ID;
+//   const notion = new Client({ auth: secret });
+//   if (!secret || !databaseId)
+//     throw new Error("Missing notion secret or DB-ID.");
 
-  const query = await notion.databases.query({ database_id: databaseId });
+//   const query = await notion.databases.query({ database_id: databaseId });
 
-  return query;
-}
+//   return query;
+// }
+// export default async function Home() {
+//   const data = await getData();
+//   return (
+//     <div>
+//       <ul>
+//         {data.results.map((project) => {
+//           if ("properties" in project) {
+//             if ("title" in project.properties.Name) {
+//               const title = project.properties.Name.title[0].plain_text;
+//               const id = project.id;
+//               return <li key={id}>{title}</li>;
+//             }
+//           }
+//         })}
+//       </ul>
+//     </div>
+//   );
+// }
+
 export default async function Home() {
-  const data = await getData();
   return (
-    <div>
-      <ul>
-        {data.results.map((project) => {
-          if ("properties" in project) {
-            if ("title" in project.properties.Name) {
-              const title = project.properties.Name.title[0].plain_text;
-              const id = project.id;
-              return <li key={id}>{title}</li>;
-            }
-          }
-        })}
-      </ul>
+    <div
+      style={{
+        display: "flex",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h2>Under Construction 💪</h2>
     </div>
   );
 }
